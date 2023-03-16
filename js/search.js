26c5d16a6b4909161e35fed5cf93415a7f329a5c
Nevaeh Maldonado <111463121+nevaehhm14@users.noreply.github.com>
2023-03-16 21:48:31 +0000

    Thu, Mar 16, 2023, 2:48 PM -07:00

--- js/search.js
+++ js/search.js
@@ -1,7 +1,9 @@
-button = document.querySelector('.btn')
-search = document.querySelector('.search')
+const button = document.querySelector('.btn')
+const search = document.querySelector('.search')
+const input = document.querySelector('.input')
 
 
 button.addEventListener('click', () => {
     search.classList.toggle('active')
+    input.focus()
 })
